2ce1ec9ebedbc8d2ec6a25032ce9467e41f15706
Paul Boyd <paul@pboyd.io>
2026-02-04 21:17:44 -0500

    fix: define MAP_32BIT for Darwin

--- syscalls_unix.go
+++ syscalls_unix.go
@@ -33,9 +33,12 @@ func mprotect(buf []byte, flags int) error {
 	return syscall.Mprotect(region, flags)
 }
 
+// Not defined for GOOS=darwin for some reason
+const map_32bit = 0x40
+
 func mmap(size int, prot int) ([]byte, error) {
 	pageSize := syscall.Getpagesize()
 	size = (size + pageSize - 1) &^ (pageSize - 1)
 
-	return syscall.Mmap(-1, 0, size, prot, syscall.MAP_PRIVATE|syscall.MAP_ANON|syscall.MAP_32BIT)
+	return syscall.Mmap(-1, 0, size, prot, syscall.MAP_PRIVATE|syscall.MAP_ANON|map_32bit)
 }
