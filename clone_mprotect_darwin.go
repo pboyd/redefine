05985f33660e176f2df3abc06b27e74f1cb430d7
Paul Boyd <paul@pboyd.io>
2026-03-08 06:55:45 -0400

    fix(clone): don't call mprotect on Darwin (#11)
    pthread_jit_write_protect_np allows writes for the thread, so just use
    that instead.

--- clone_mprotect_darwin.go
+++ clone_mprotect_darwin.go
@@ -2,28 +2,33 @@
 
 package redefine
 
-import "golang.org/x/sys/unix"
-
 /*
-	#include <pthread.h>
+#include <pthread.h>
 */
 import "C"
+import (
+	"runtime"
+
+	"golang.org/x/sys/unix"
+)
 
 func mprotectHook(inner func(int) error) func(int) error {
 	return func(prot int) error {
+		// Instead of calling mprotect, just use Darwin's
+		// pthread_jit_write_protect_np which is effectively the same
+		// in this case.
+
+		// This value is thread specific, so lock the running goroutine
+		// to the system thread. This assumes that this function is
+		// called in BeginMutate/EndMutate pairs.
+
 		if prot&unix.PROT_WRITE != 0 {
+			runtime.LockOSThread()
 			C.pthread_jit_write_protect_np(0)
 		} else {
 			C.pthread_jit_write_protect_np(1)
+			runtime.UnlockOSThread()
 		}
-
-		err := inner(prot)
-
-		// Restore write protection after an error
-		if err != nil && prot&unix.PROT_WRITE != 0 {
-			C.pthread_jit_write_protect_np(1)
-		}
-
-		return err
+		return nil
 	}
 }
