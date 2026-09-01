47418dda2e34e8ad0c725a8dcecd0f5f1156fc27
Paul Boyd <paul@pboyd.io>
2026-09-01 18:42:12 -0400

    chore: update ExampleOriginal for Go 1.27
    json.Marshal appears to be inlined now, updating the example to
    compensate.

--- example_amd64_test.go
+++ example_amd64_test.go
@@ -9,21 +9,26 @@ import (
 	"github.com/pboyd/redefine"
 )
 
+//go:noinline
+func toJSON(v any) ([]byte, error) {
+	return json.Marshal(v)
+}
+
 func ExampleOriginal() {
-	redefine.Func(json.Marshal, func(v any) ([]byte, error) {
+	redefine.Func(toJSON, func(v any) ([]byte, error) {
 		// Pass strings through
 		if _, ok := v.(string); ok {
-			return redefine.Original(json.Marshal)(v)
+			return redefine.Original(toJSON)(v)
 		}
 
 		return []byte(`{"nah": true}`), nil
 	})
-	defer redefine.Restore(json.Marshal)
+	defer redefine.Restore(toJSON)
 
-	buf, _ := json.Marshal("A string")
+	buf, _ := toJSON("A string")
 	fmt.Println(string(buf))
 
-	buf, _ = json.Marshal(123)
+	buf, _ = toJSON(123)
 	fmt.Println(string(buf))
 	// Output:
 	// "A string"
